67bf3c41494144062f909d87a1a31d8468969981
mott2019 <vendasobambu@gmail.com>
2021-01-21 11:21:25 -0300

    Projeto criado v1

---
+++ src/main/java/com/andremott/cursomc/CursoandApplication.java
@@ -0,0 +1,13 @@
+package com.andremott.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoandApplication.class, args);
+	}
+
+}
