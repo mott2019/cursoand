41de65fab5de415d519f0146e1a3a7330753ed30
mott2019 <vendasobambu@gmail.com>
2021-01-21 15:22:53 -0300

    Criando operação de instanciação - Categoria

--- src/main/java/com/andremott/cursomc/CursoandApplication.java
+++ src/main/java/com/andremott/cursomc/CursoandApplication.java
@@ -1,13 +1,34 @@
 package com.andremott.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.andremott.cursomc.domain.Categoria;
+import com.andremott.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursoandApplication {
+public class CursoandApplication implements CommandLineRunner {
+	
+	/*salvar o objeto no banco de dados*/
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoandApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		 Categoria cat1 = new Categoria(null, "Informática");
+		 Categoria cat2 = new Categoria(null, "Escritório");
+		 
+		 categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		 
 	}
 
 }
