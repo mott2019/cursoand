d85ee3386dc8e7d9dda92eaeff7cfa0559bcb6ca
mott2019 <vendasobambu@gmail.com>
2021-01-21 15:05:00 -0300

    Criando Repository e Service para Categoria

--- src/main/java/com/andremott/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/andremott/cursomc/resources/CategoriaResource.java
@@ -3,11 +3,15 @@ package com.andremott.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.andremott.cursomc.domain.Categoria;
+import com.andremott.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -24,6 +28,15 @@ public class CategoriaResource {
 		lista.add(cat2);
 		
 		return lista; 
+	}
+	
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+    public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
