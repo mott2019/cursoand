a2b389c641db6d86b5b68e5e109a05ed35ef58af
mott2019 <vendasobambu@gmail.com>
2021-01-22 08:41:33 -0300

    Endpoint / clientes / enderecos

---
+++ src/main/java/com/andremott/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.andremott.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andremott.cursomc.domain.Cliente;
+import com.andremott.cursomc.repositories.ClienteRepository;
+import com.andremott.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+		//return obj.orElse(null);
+	}
+	
+}
